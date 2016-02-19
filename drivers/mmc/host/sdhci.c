2186920c37e02a70cec32813efe26d166e286796
Tobias Jakobi <tjakobi@math.uni-bielefeld.de>
2016-02-19 15:44:07 +0100

    mmc: sdhci: Handle 2.8~2.9 volts range in sdhci_set_power()

--- drivers/mmc/host/sdhci.c
+++ drivers/mmc/host/sdhci.c
@@ -1729,6 +1729,7 @@ void sdhci_set_power_noreg(struct sdhci_host *host, unsigned char mode,
 		case MMC_VDD_20_21:
 			pwr = SDHCI_POWER_180;
 			break;
+		case MMC_VDD_28_29:
 		case MMC_VDD_29_30:
 		case MMC_VDD_30_31:
 			pwr = SDHCI_POWER_300;
