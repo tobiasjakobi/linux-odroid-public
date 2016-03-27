6b302572ac877b86106f34c9b3b8a2d98a5f788d
Tobias Jakobi <tjakobi@math.uni-bielefeld.de>
2016-03-28 01:01:08 +0200

    drm/exynos: g2d: remove EXYNOS_G2D_SET_CMDLIST ioctl
    The ioctl is going to be replaced by a more flexible
    version. It's easier to first remove the ioctl (and
    everything related to it), than to introduce the new
    version side-by-side.

--- drivers/gpu/drm/exynos/exynos_drm_g2d.h
+++ drivers/gpu/drm/exynos/exynos_drm_g2d.h
@@ -7,8 +7,6 @@
 #ifdef CONFIG_DRM_EXYNOS_G2D
 extern int exynos_g2d_get_ver2_ioctl(struct drm_device *dev, void *data,
 				    struct drm_file *file_priv);
-extern int exynos_g2d_set_cmdlist_ioctl(struct drm_device *dev, void *data,
-					struct drm_file *file_priv);
 extern int exynos_g2d_exec_ioctl(struct drm_device *dev, void *data,
 				 struct drm_file *file_priv);
 extern int exynos_g2d_userptr_ioctl(struct drm_device *dev, void *data,
@@ -19,13 +17,6 @@ extern void g2d_close(struct drm_device *drm_dev, struct drm_file *file);
 #else
 static inline int exynos_g2d_get_ver2_ioctl(struct drm_device *dev, void *data,
 					   struct drm_file *file_priv)
-{
-	return -ENODEV;
-}
-
-static inline int exynos_g2d_set_cmdlist_ioctl(struct drm_device *dev,
-					       void *data,
-					       struct drm_file *file_priv)
 {
 	return -ENODEV;
 }
