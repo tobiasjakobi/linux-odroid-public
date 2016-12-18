969c38e9f3c1e77e3d16344620defc08bab2f792
Tobias Jakobi <tjakobi@math.uni-bielefeld.de>
2016-12-18 17:23:41 +0100

    PM / devfreq: Add support for turbo operation
    During turbo the DevFreq device operates with the highest
    OPP state. Turbo operation is reference counted, so
    multiple device drivers can request it.

--- include/linux/devfreq.h
+++ include/linux/devfreq.h
@@ -171,6 +171,8 @@ struct devfreq {
 	struct notifier_block nb;
 	struct delayed_work work;
 
+	unsigned int turbo_refcount;
+
 	unsigned long previous_freq;
 	struct devfreq_dev_status last_status;
 
@@ -211,6 +213,9 @@ struct devfreq *devm_devfreq_add_device(struct device *dev,
 				const char *governor_name,
 				void *data);
 void devm_devfreq_remove_device(struct device *dev, struct devfreq *devfreq);
+
+extern int devfreq_turbo_get(struct devfreq *devfreq);
+extern int devfreq_turbo_put(struct devfreq *devfreq);
 
 /* Supposed to be called by PM callbacks */
 int devfreq_suspend_device(struct devfreq *devfreq);
@@ -332,6 +337,16 @@ static inline struct devfreq *devm_devfreq_add_device(struct device *dev,
 static inline void devm_devfreq_remove_device(struct device *dev,
 					struct devfreq *devfreq)
 {
+}
+
+static inline int devfreq_turbo_get(struct devfreq *devfreq)
+{
+	return 0;
+}
+
+static inline int devfreq_turbo_put(struct devfreq *devfreq)
+{
+	return 0;
 }
 
 static inline int devfreq_suspend_device(struct devfreq *devfreq)
