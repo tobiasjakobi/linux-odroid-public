9b73651c571f14e3f7ecb87f4bb9147769773249
Tobias Jakobi <tjakobi@math.uni-bielefeld.de>
2017-08-07 15:26:14 +0200

    mali: fix use of GFP flags
    Replace with __GFP_REPEAT with __GFP_RETRY_MAYFAIL. __GFP_REPEAT
    was removed from upstream.
    
    Upstream change:
    dcda9b04713c3f6ff0875652924844fae28286ea

--- drivers/gpu/arm/mali/linux/mali_memory_os_alloc.c
+++ drivers/gpu/arm/mali/linux/mali_memory_os_alloc.c
@@ -119,7 +119,7 @@ static int mali_mem_os_alloc_pages(mali_mem_allocation *descriptor, u32 size)
 	/* Allocate new pages, if needed. */
 	for (i = 0; i < remaining; i++) {
 		dma_addr_t dma_addr;
-		gfp_t flags = __GFP_ZERO | __GFP_REPEAT | __GFP_NOWARN | __GFP_COLD;
+		gfp_t flags = __GFP_ZERO | __GFP_RETRY_MAYFAIL | __GFP_NOWARN | __GFP_COLD;
 		int err;
 
 #if defined(CONFIG_ARM) && !defined(CONFIG_ARM_LPAE)
